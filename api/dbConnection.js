c999e85d6a3801095b3659868884cbae701932f5
uthayasakthivel <uthayasakthivel8@gmail.com>
2025-01-17 16:24:50 +0530

    env changes

--- api/dbConnection.js
+++ api/dbConnection.js
@@ -19,4 +19,5 @@ export const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
